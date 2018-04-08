ce31d4cf37a290ffd61d43a10f507072bda24162
Ahmed ABDELFATAH <ahmed.abdelfatah@valeo.com>
2018-04-09 01:14:08 +0200

    v 8.0

--- hello.c
+++ hello.c
@@ -5,10 +5,8 @@ void main ()
 
 	printf("Hello C and Git \n");
 	printf("Ahmed");
-<<<<<<< HEAD
+
 	printf ("TTTTTTTTTT");
 	printf("YYYYYYYYY");
-=======
 
->>>>>>> 2c0b756a32d360a2ba70edd81697c5d6c72a97ec
 }
